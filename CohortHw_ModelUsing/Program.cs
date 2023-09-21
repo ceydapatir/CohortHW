b16603b603f82d326f37aaabfe29e2c6bce92eec
Ceyda Patır <patirceyda@gmail.com>
2023-09-22 00:26:26 +0300

    Update Hw

--- CohortHw_ModelUsing/Program.cs
+++ CohortHw_ModelUsing/Program.cs
@@ -1,3 +1,6 @@
+using CohortHw_ModelUsing.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//DB
+builder.Services.AddDbContext<MovieDbContext>(options => options.UseInMemoryDatabase(databaseName: "MovieArchiveDB"));
 
 var app = builder.Build();
 
